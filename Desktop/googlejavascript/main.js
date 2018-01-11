08911f73b3568b992ec4f7b65a6322797eca06b9
Arnaud <arnaud.meneroud@gmail.com>
2018-01-11 18:49:20 +0000

    Update

---
+++ Desktop/googlejavascript/main.js
@@ -0,0 +1,10 @@
+function showDiv() {
+
+   document.getElementById('centre').style.display = "block"; /* Depuis OnClick fait appel a la section "Centre" qui est cachée et affiche le block */
+   document.getElementById('bouton').style.display= "none"; 
+   /*  fait disparaitre le bouton*/
+
+}
+
+setTimeout(function() {
+alert()} , 10000);
